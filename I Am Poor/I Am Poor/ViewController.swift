fddb36160be2ba63d6e50a7446a8d696087b47ed
sebasao92 <sealor92@gmail.com>
2019-07-14 13:52:22 -0500

    projects created based on iOS udemy course
    first commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sebas-MacOS on 6/30/19.
+//  Copyright © 2019 Sealor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
